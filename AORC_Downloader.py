74496e47bec37e07e895ee49a63fba68867e68bd
Myles <mmcmanus@thewaterinstitute.org>
2022-08-24 09:46:13 -0500

    init

--- AORC_Downloader.py
+++ AORC_Downloader.py
@@ -1,5 +1,7 @@
 # This script can be used to download hourly AORC precip data from the web.
 # It is currently setup to download from the AORC_LMRFC_4km repository for Hurricane Ida Aug 26, 2021 – Sep 4, 2021.
+# The zip files are compressed hourly netCDF files by month.
+# Ex: AORC_APCP_4KM_LMRFC_202101.zip will contain every hourly netcdf file for January, 2021.
 
 import zipfile
 import requests
